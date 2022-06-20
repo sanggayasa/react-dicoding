1b4194c1602f2ad029eb20d61c1598a65386d4d5
sanggayasa <sanggayasa502@gmail.com>
2022-06-20 14:08:45 +0700

    first

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createRoot } from 'react-dom/client';
+import ContactApp from './components/ContactApp';
+import './style/style.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class MyComponent extends React.Component {
+
+    constructor(props) {
+        super(props);
+        console.log('Component created!');
+
+    }
+
+    render() {
+        return <div className='conatiner'>
+
+            <p>Halaman, {this.props.name} </p>
+        <ContactApp /></div>;
+    }
+}
+
+const root = createRoot(document.getElementById('root'));
+root.render(<div><MyComponent name="Dicoding"/></div>);
